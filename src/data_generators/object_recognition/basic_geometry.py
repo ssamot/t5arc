Fixed the __copy__ of all  objects and corrected the symmetry calculations after scaling

src/data_generators/object_recognition/basic_geometry.py
@@ -4,7 +4,7 @@ from __future__ import annotations
 import numpy as np
 import constants as const
 from typing import Union, List
-from dataclasses import dataclass
+from copy import copy
 from enum import Enum
 from dataclasses import dataclass
 
@@ -42,6 +42,8 @@ class Orientation(Enum):
 
         return Orientation(value=value)
 
+    def __copy__(self):
+        return Orientation(self.value)
 
 class OrientationZ(Enum):
     Away: int = -1
@@ -73,8 +75,12 @@ class Surround:
     def __isub__(self, other: Surround | int) -> Surround:
         return self - other
 
+    def __copy__(self):
+        return Surround(Up=self.Up, Down=self.Down, Left=self.Left, Right=self.Right)
+
     def to_numpy(self):
         return np.array([self.Up, self.Down, self.Left, self.Right])
+
 
 class Dimension2D:
     def __init__(self, dx: int = 3, dy:  int = 3, array: None | np.ndarray | List = None):
@@ -125,6 +131,9 @@ class Dimension2D:
 
     def __truediv__(self, other) -> Dimension2D:
         return Dimension2D(self.x / other, self.y / other)
+
+    def __copy__(self):
+        return Dimension2D(dx=self.dx, dy=self.dy)
 
     def to_numpy(self):
         return np.array([self.dx, self.dy])
@@ -187,8 +196,11 @@ class Point:
     def __len__(self):
         return 3
 
-    def __deepcopy__(self, memo) -> Point:
+    def __copy__(self) -> Point:
         return Point(self.x, self.y, self.z)
+
+    def __abs__(self):
+        return Point(np.abs(self.x), np.abs(self.y), np.abs(self.z))
 
     def to_numpy(self) -> np.ndarray:
         return np.array([self.x, self.y, self.z])
@@ -258,6 +270,7 @@ class Point:
     def copy(self) -> Point:
         return Point(self.x, self.y, self.z)
 
+
 class Vector:
     def __init__(self, orientation:Orientation = Orientation.Up,
                  length: None | int = 0,
@@ -269,8 +282,8 @@ class Vector:
     def __repr__(self):
         return f'Vector(Orientation: {self.orientation}, Length: {self.length}, Origin Point: {self.origin})'
 
-    def __deepcopy__(self, memo):
-        return self
+    def __copy__(self):
+        return Vector(orientation=copy(self.orientation), length=self.length, origin=copy(self.origin))
 
     # TODO: Need to deal with transformations other than rotation
     def transform(self, affine_matrix: np.ndarray | None = None,
@@ -287,9 +300,6 @@ class Vector:
 
         self.origin.transform(affine_matrix, rotation, shear, translation, scale)
 
-    def copy(self) -> Vector:
-        return Vector(orientation=self.orientation, origin=self.origin.copy(), length=self.length)
-
 
 class Bbox:
     def __init__(self, top_left: Point = Point(0, 0), bottom_right: Point = Point(0, 0)):
@@ -301,8 +311,8 @@ class Bbox:
         self.center = self._calculate_center()
         return self.center
 
-    def __deepcopy__(self, memo):
-        return self
+    def __copy__(self):
+        return Bbox(top_left=self.top_left, bottom_right=self.bottom_right)
 
     def __repr__(self):
         return f'Bbox(Top Left: {self.top_left}, Bottom Right: {self.bottom_right}, Center: {self.center})'
@@ -334,22 +344,22 @@ class Bbox:
             rot = 3
 
         if rot == 1:
-            p1 = self.top_left
+            p1 = copy(self.top_left)
             p1.transform(affine_matrix, rotation, shear, translation, scale)
-            p2 = self.bottom_right
+            p2 = copy(self.bottom_right)
             p2.transform(affine_matrix, rotation, shear, translation, scale)
             self.top_left = Point.point_from_numpy(np.array([p1.x, p2.y, 0]))
             self.bottom_right = Point.point_from_numpy(np.array([p2.x, p1.y, 0]))
 
         elif rot == 2:
-            p = self.top_left
+            p = copy(self.top_left)
             self.top_left = Point(self.bottom_right.x - 2*(self.bottom_right.x - self.top_left.x), self.top_left.x)
             self.bottom_right = Point(p.x, p.y - 2 * (p.y - self.bottom_right.y))
 
         elif rot == 3:
-            p1 = self.top_left
+            p1 = copy(self.top_left)
             p1.transform(affine_matrix, rotation, shear, translation, scale)
-            p2 = self.bottom_right
+            p2 = copy(self.bottom_right)
             p2.transform(affine_matrix, rotation, shear, translation, scale)
 
             self.top_left = Point.point_from_numpy(np.array([p2.x, p1.y, 0]))

src/data_generators/object_recognition/object.py
@@ -56,6 +56,9 @@ class Object:
         :return: Nothing
         """
 
+        def length_multiplier(factor):
+            return factor + factor - 2
+
         if factor == 0:  # Factor cannot be 0 so in this case nothing happens
             return
 
@@ -80,20 +83,25 @@ class Object:
 
         self.actual_pixels = scaled
 
+        self.reset_dimensions()
+
         for sym in self.symmetries:
             if factor > 0:
-                edges = Point(0.5, 0.5)
-                if sym.origin.x == 0:
-                    edges.x = 0
-                if sym.origin.y == 0:
-                    edges.y = 0
-                sym.origin = (sym.origin + edges - self._canvas_pos) * factor + self._canvas_pos
+                sym.length *= 2 * factor - 0.5 * (factor + 1)
+
+                d = np.abs(sym.origin - self.canvas_pos)
+                multiplier = Point(factor, factor) if sym.orientation == Orientation.Up else Point(factor + 1, factor)
+                sym.origin.x = d.x * multiplier.x + self.canvas_pos.x if d.x > 0 else sym.origin.x
+                sym.origin.y = d.y * multiplier.y  + self.canvas_pos.y if d.y > 0 else sym.origin.y
+
+                sym.origin.x += 0.5 * (factor -1) if sym.orientation == Orientation.Up\
+                    else -(factor -1) + (factor - 2) *2
+                sym.origin.y += (factor - 1) * 0.5 if sym.orientation == Orientation.Left\
+                    else (factor - 1) * 0.5 - (factor - 2) * 0.5 - 0.5
             else:
+                sym.length /= np.abs(factor)
                 factor = 1/np.abs(factor)
                 sym.origin = (sym.origin - self._canvas_pos) * factor + self._canvas_pos
-            sym.length *= factor
-
-        self.reset_dimensions()
 
     def rotate(self, times: Union[1, 2, 3], center: np.ndarray | List | Point = (0, 0)):
         """
@@ -103,7 +111,7 @@ class Object:
         :return:
         """
         radians = np.pi/2 * times
-        degrees = 90 * times
+        degrees = -(90 * times)
         self.actual_pixels = skimage.transform.rotate(self.actual_pixels, degrees, resize=True, order=0, center=center)
 
         if type(center) == Point:
@@ -294,11 +302,15 @@ class Object:
 
         self.bbox = Bbox(top_left=bb_top_left, bottom_right=bb_bottom_right)
 
-    def copy(self):
+    def __copy__(self):
         new_obj = Object(actual_pixels=self.actual_pixels, _id=self.id, border_size=self.border_size,
                          canvas_pos=self.canvas_pos)
+        new_obj.dimensions = copy(self.dimensions)
+        new_obj.border_size = copy(self.border_size)
+        new_obj.bbox = copy(self.bbox)
+        new_obj.rotation_axis = copy(self.rotation_axis)
         for sym in self.symmetries:
-            new_obj.symmetries.append(sym.copy())
+            new_obj.symmetries.append(copy(sym))
         return new_obj
 
     def __add__(self, other: Object):

src/data_generators/object_recognition/primitives.py
@@ -1,6 +1,8 @@
 
 from __future__ import annotations
 
+from copy import copy
+
 import numpy as np
 import constants as const
 from typing import List
@@ -111,7 +113,11 @@ class Primitive(Object):
             x_symmetry = Vector(orientation=x_sym_or, length=x_sym_length, origin=x_sym_origin)
             self.symmetries.append(x_symmetry)
 
-    def copy(self):
+    def reset_dimensions(self):
+        Object.reset_dimensions(self)
+        self.size = self.dimensions
+
+    def __copy__(self):
         args = self.__dict__.copy()
         for arg in ['actual_pixels', 'border_size', '_canvas_pos', 'id', 'rotation_axis', 'dimensions',
                     'number_of_coloured_pixels', 'symmetries', 'bbox']:
@@ -126,7 +132,16 @@ class Primitive(Object):
             args['border_size'] = self.border_size
 
         object = type(self)(**args)
-        object.actual_pixels[:, :] = self.actual_pixels[:, :]
+        object.canvas_pos = Point(self.canvas_pos.x, self.canvas_pos.y, self.canvas_pos.z)
+        object.dimensions = Dimension2D(self.dimensions.dx, self.dimensions.dy)
+        object.actual_pixels = np.ndarray.copy(self.actual_pixels)
+        object.symmetries = []
+        for sym in self.symmetries:
+            object.symmetries.append(copy(sym))
+        object.border_size = Surround(Up=self.border_size.Up, Down=self.border_size.Down, Left=self.border_size.Left,
+                                      Right=self.border_size.Right)
+        object.rotation_axis = Point(self.rotation_axis.x, self.rotation_axis.y, self.rotation_axis.z)
+        object.reset_dimensions()
         return object
 
 
@@ -263,7 +278,7 @@ class Pi(Primitive):
         A Pi shaped object.
         :param size: The x, y size of the object
         :param border_size: The [Up, Down, Left, Right] black pixels surrounding the Pi
-        :param canvas_pos: The position on the canvas
+        :param _canvas_pos: The position on the canvas
         :param colour: The Pi's colour
         :param _id: The id of the object
         """
@@ -283,8 +298,7 @@ class Pi(Primitive):
         pi = np.array(pi)
 
         self.generate_actual_pixels(array=pi)
-
-        Object.__init__(self, actual_pixels=self.actual_pixels, canvas_pos=canvas_pos, border_size=border_size,  _id=_id)
+        Object.__init__(self, actual_pixels=self.actual_pixels, canvas_pos=canvas_pos, border_size=border_size, _id=_id)
 
         self.generate_symmetries('y')
 