ea6ebbab0c7cbdc16ebcb8e67c4a3656c079117c
georgedimitriadis <gdimitri@hotmail.com>
2024-08-22 10:30:02 +0100

    Some debugging to deal with extra input cases

--- src/dsls/our_dsl/functions/dsl_functions.py
+++ src/dsls/our_dsl/functions/dsl_functions.py
@@ -4,28 +4,31 @@ from typing import List
 from data_generators.object_recognition.basic_geometry import Point, Vector, Dimension2D
 
 
-def furthest(origin: Point, targets: List[Point]) -> Vector | None:
+def furthest(origin: Point, targets: List[Point] | Point) -> Vector | None:
+    if type(targets) == Point:
+        return origin.euclidean_distance(targets)
     result = targets[0]
     for t in targets[1:]:
-        if origin.manhattan_direction(result) is None:
+        if origin.euclidean_distance(result) is None:
             result = t
-        elif origin.manhattan_direction(t) is not None:
-            if origin.manhattan_direction(t).length > origin.manhattan_direction(result).length:
+        elif origin.euclidean_distance(t) is not None:
+            if origin.euclidean_distance(t).length > origin.euclidean_distance(result).length:
                 result = t
 
-    return origin.manhattan_direction(result)
+    return origin.euclidean_distance(result)
 
 
 def closest(origin: Point, targets: List[Point]):
+    if type(targets) == Point:
+        return origin.euclidean_distance(targets)
     result = targets[0]
     for t in targets[1:]:
-        if origin.manhattan_direction(result) is None:
+        if origin.euclidean_distance(result) is None:
             result = t
-        elif origin.manhattan_direction(t) is not None:
-            if origin.manhattan_direction(t).length < origin.manhattan_direction(result).length:
+        elif origin.euclidean_distance(t) is not None:
+            if origin.euclidean_distance(t).length < origin.euclidean_distance(result).length:
                 result = t
-
-    return origin.manhattan_direction(result)
+    return origin.euclidean_distance(result)
 
 
 def loop():
