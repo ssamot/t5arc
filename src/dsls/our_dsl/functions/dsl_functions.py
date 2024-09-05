22e66a515fbf59596fed868d81aa4a29151fc051
georgedimitriadis <gdimitri@hotmail.com>
2024-09-05 14:20:09 +0100

    Expanding the dsl. All 4 working examples have been transferred over to the new functional dsl

--- src/dsls/our_dsl/functions/dsl_functions.py
+++ src/dsls/our_dsl/functions/dsl_functions.py
@@ -1,65 +1,291 @@
 
 from copy import copy
-from typing import List
+from typing import List, Tuple
 
-from data.generators.object_recognition.basic_geometry import Point, Vector
+import numpy as np
+
+from data.generators.object_recognition.basic_geometry import Point, Vector, Dimension2D, RelativePoint, Orientation, \
+    Surround
 from data.generators.object_recognition.canvas import Canvas
 from data.generators.object_recognition.object import Object
+from data.generators.object_recognition.primitives import Primitive
 
 
-def furthest(origin: Point, targets: List[Point] | Point) -> Vector | None:
+#  Funcs on Points
+def furthest_point_to_point(origin: Point, targets: List[Point] | Point) -> Tuple[Vector, int]:
     if type(targets) == Point:
-        return origin.euclidean_distance(targets)
+        return origin.euclidean_distance(targets), 0
     result = targets[0]
-    for t in targets[1:]:
+    index = 0
+    for i, t in enumerate(targets):
         if origin.euclidean_distance(result) is None:
             result = t
+            index = i
         elif origin.euclidean_distance(t) is not None:
             if origin.euclidean_distance(t).length > origin.euclidean_distance(result).length:
                 result = t
+                index = i
 
-    return origin.euclidean_distance(result)
+    return origin.euclidean_distance(result), index
 
 
-def closest(origin: Point, targets: List[Point]) -> Vector | None:
+def closest_point_to_point(origin: Point, targets: List[Point] | Point) -> Tuple[Vector, int]:
     if type(targets) == Point:
-        return origin.euclidean_distance(targets)
+        return origin.euclidean_distance(targets), 0
     result = targets[0]
-    for t in targets[1:]:
+    index = 0
+    for i, t in enumerate(targets):
         if origin.euclidean_distance(result) is None:
             result = t
+            index = i
         elif origin.euclidean_distance(t) is not None:
             if origin.euclidean_distance(t).length < origin.euclidean_distance(result).length:
                 result = t
-    return origin.euclidean_distance(result)
+                index = i
+
+    return origin.euclidean_distance(result), index
 
 
-def largest_object_by_area(canvas: Canvas) -> Object:
-    return canvas.sort_objects_by_size(used_dim='area')[-1]
+def sum_points(first: Point, second: Point) -> Point:
+    f = copy(first)
+    s = copy(second)
+    return f + s
 
 
-def largest_object_by_height(canvas: Canvas) -> Object:
-    return canvas.sort_objects_by_size(used_dim='height')[-1]
+def subtract_points(first: Point, second: Point) -> Point:
+    f = copy(first)
+    s = copy(second)
+    return f - s
 
 
-def largest_object_by_width(canvas: Canvas) -> Object:
-    return canvas.sort_objects_by_size(used_dim='width')[-1]
+#  Funcs on Canvasses
+def copy_canvas(canvas: Canvas) -> Canvas:
+    return copy(canvas)
 
 
-def smallest_object_by_area(canvas: Canvas) -> Object:
-    return canvas.sort_objects_by_size(used_dim='area')[0]
+def copy_object(obj: Primitive) -> Primitive:
+    return copy(obj)
 
 
-def smallest_object_by_height(canvas: Canvas) -> Object:
-    return canvas.sort_objects_by_size(used_dim='height')[0]
+def make_new_canvas_as(canvas: Canvas) -> Canvas:
+    new_canvas = Canvas(size=canvas.size)
+    return new_canvas
 
 
-def smallest_object_by_width(canvas: Canvas) -> Object:
-    return canvas.sort_objects_by_size(used_dim='width')[0]
+def make_new_canvas(size: Dimension2D) -> Canvas:
+    return Canvas(size=size)
 
 
-def rest_of_the_objects(canvas: Canvas, obj: Object):
-    temp_obj_list = copy(canvas.objects)
-    temp_obj_list.remove(obj)
+def add_object_to_canvas(canvas: Canvas, obj: Primitive) -> Canvas:
+    new_canvas = copy(canvas)
+    new_obj = copy(obj)
+    new_canvas.add_new_object(new_obj)
+    return new_canvas
+
+
+# Funcs to get Primitive features
+def get_distance_min_between_objects(first: Primitive, second: Primitive) -> Vector:
+    return first.distance_to_object(other=second, dist_type='min')
+
+
+def get_distance_max_between_objects(first: Primitive, second: Primitive) -> Vector:
+    return first.distance_to_object(other=second, dist_type='max')
+
+
+def get_distance_origin_to_origin_between_objects(first: Primitive, second: Primitive) -> Vector:
+    return first.distance_to_object(other=second, dist_type='canvas_pos')
+
+
+def get_distance_touching_between_objects(first: Primitive, second: Primitive) -> Vector:
+    dist = get_distance_min_between_objects(first=first, second=second)
+    dist.length -= 1
+    return dist
+
+
+def get_point_for_match_shape_furthest(base_obj: Primitive, target_obj: Primitive, match_shape_only: bool,
+                                       padding: Surround = Surround(0, 0, 0, 0)) -> Point:
+    match_positions, _ = base_obj.match(target_obj, after_rotation=True,
+                                                match_shape_only=match_shape_only, padding=padding)
+
+    _, index = furthest_point_to_point(base_obj.canvas_pos, match_positions)
+
+    return match_positions[index]
+
+
+def get_point_and_rotation_for_match_shape_furthest(base_obj: Primitive, target_obj: Primitive, match_shape_only: bool,
+                                                    padding: Surround = Surround(0, 0, 0, 0)) -> Tuple[Point, int]:
+    match_positions, rotations = base_obj.match(target_obj, after_rotation=True,
+                                                match_shape_only=match_shape_only, padding=padding)
+
+    _, index = furthest_point_to_point(base_obj.canvas_pos, match_positions)
+
+    return match_positions[index], rotations[index]
+
+
+def get_point_for_match_shape_nearest(base_obj: Primitive, target_obj: Primitive, match_shape_only: bool,
+                                      padding: Surround = Surround(0, 0, 0, 0)) -> Point:
+    match_positions, _ = base_obj.match(target_obj, after_rotation=True,
+                                                match_shape_only=match_shape_only, padding=padding)
+
+    _, index = closest_point_to_point(base_obj.canvas_pos, match_positions)
+
+    return match_positions[index]
+
+
+def get_point_and_rotation_for_match_shape_nearest(base_obj: Primitive, target_obj: Primitive, match_shape_only: bool,
+                                                   padding: Surround = Surround(0, 0, 0, 0)) -> Tuple[Point, int]:
+    match_positions, rotations = base_obj.match(target_obj, after_rotation=True,
+                                                match_shape_only=match_shape_only, padding=padding)
+
+    _, index = closest_point_to_point(base_obj.canvas_pos, match_positions)
+
+    return match_positions[index], rotations[index]
+
+
+def get_object_feature_colour(obj: Primitive) -> int:
+    return obj.colour
+
+
+def get_object_feature_size(obj: Primitive) -> Dimension2D:
+    return obj.size
+
+
+def get_object_feature_canvas_pos(obj: Primitive) -> Point:
+    return obj.canvas_pos
+
+
+# Funcs to select Primitives
+def select_largest_object_by_area(canvas: Canvas) -> Primitive:
+    new_canvas = copy(canvas)
+    return new_canvas.sort_objects_by_size(used_dim='area')[-1]
+
+
+def select_largest_object_by_height(canvas: Canvas) -> Primitive:
+    new_canvas = copy(canvas)
+    return new_canvas.sort_objects_by_size(used_dim='height')[-1]
+
+
+def select_largest_object_by_width(canvas: Canvas) -> Primitive:
+    new_canvas = copy(canvas)
+    return new_canvas.sort_objects_by_size(used_dim='width')[-1]
+
+
+def select_smallest_object_by_area(canvas: Canvas) -> Primitive:
+    new_canvas = copy(canvas)
+    return new_canvas.sort_objects_by_size(used_dim='area')[0]
+
+
+def select_smallest_object_by_height(canvas: Canvas) -> Primitive:
+    new_canvas = copy(canvas)
+    return new_canvas.sort_objects_by_size(used_dim='height')[0]
+
+
+def select_smallest_object_by_width(canvas: Canvas) -> Primitive:
+    new_canvas = copy(canvas)
+    return new_canvas.sort_objects_by_size(used_dim='width')[0]
+
+
+def select_rest_of_the_objects(canvas: Canvas, obj: Primitive | None) -> List[Primitive]:
+    temp_obj_list = [copy(o) for o in canvas.objects]
+    if obj is not None:
+        temp_obj_list.remove(obj)
     return temp_obj_list
 
+
+def select_object_of_colour(canvas: Canvas, colour: int) -> Primitive:
+    new_canvas = copy(canvas)
+    return new_canvas.find_objects_of_colour(colour)[0]
+
+
+# Funcs to transform Primitives
+def object_transform_rotate(obj: Primitive, rotation: int) -> Primitive:
+    new_obj = copy(obj)
+    new_obj.rotate(times=rotation)
+    return new_obj
+
+
+def object_transform_translate_to_point(obj: Primitive, target_point: Point,
+                                        object_point: Point | None = None) -> Primitive:
+    new_obj = copy(obj)
+    new_obj.translate_to(target_point=target_point, object_point=object_point)
+    return new_obj
+
+
+def object_transform_translate_by_distance(obj: Primitive, distance: Dimension2D) -> Primitive:
+    new_obj = copy(obj)
+    new_obj.translate_by(distance=distance)
+    return new_obj
+
+
+def object_transform_translate_along_direction(obj: Primitive, direction: Vector) -> Primitive:
+    new_obj = copy(obj)
+    new_obj.translate_along_direction(direction=direction)
+    return new_obj
+
+
+def object_transform_translate_relative_point_to_point(obj: Primitive, relative_point: RelativePoint,
+                                                       other_point: Point) -> Primitive:
+    new_obj = copy(obj)
+    new_obj.translate_relative_point_to_point(relative_point=relative_point, other_point=other_point)
+    return new_obj
+
+
+def object_transform_mirror(obj: Primitive, axis: Orientation):
+    new_obj = copy(obj)
+    new_obj.mirror(axis=axis, on_axis=False)
+    return new_obj
+
+
+def object_transform_mirror_on_axis(obj: Primitive, axis: Orientation):
+    new_obj = copy(obj)
+    new_obj.mirror(axis=axis, on_axis=True)
+    return new_obj
+
+
+def object_transform_flip_only(obj: Primitive, axis: Orientation | Vector):
+    new_obj = copy(obj)
+    if isinstance(axis, Vector):
+        axis = axis.orientation
+    new_obj.flip(axis=axis, translate=False)
+    return new_obj
+
+
+def object_transform_flip_and_translate(obj: Primitive, axis: Orientation | Vector):
+    new_obj = copy(obj)
+    if isinstance(axis, Vector):
+        axis = axis.orientation
+    new_obj.flip(axis=axis, translate=True)
+    return new_obj
+
+
+def object_transform_new_colour(obj: Primitive, colour: int) -> Primitive:
+    new_obj = copy(obj)
+    new_obj.set_new_colour(new_colour=colour)
+    return new_obj
+
+
+def object_transform_negate(obj: Primitive) -> Primitive:
+    new_obj = copy(obj)
+    new_obj.negative_colour()
+    return  new_obj
+
+
+def object_transform_delete_colour(obj: Primitive, colour: int) -> Primitive:
+    new_obj = copy(obj)
+    new_obj.actual_pixels[np.where(new_obj.actual_pixels == colour)] = 1
+    return new_obj
+
+
+def split_object_by_colour(obj: Primitive) -> List[Primitive]:
+    temp_canvas = Canvas(size=obj.size)
+    o = copy(obj)
+    temp_canvas.add_new_object(o)
+    temp_canvas.split_object_by_colour(o)
+    objects = [ob for ob in temp_canvas.objects]
+    return objects
+
+
+def split_object_by_colour_on_canvas(canvas: Canvas, obj: Primitive) -> Canvas:
+    new_canvas = copy(canvas)
+    new_canvas.split_object_by_colour(obj)
+    return new_canvas
--- src/dsls/our_dsl/solutions/solutions.py
+++ src/dsls/our_dsl/solutions/solutions.py
@@ -1,89 +1,83 @@
 
-from copy import copy
-import numpy as np
 
 from data.generators.object_recognition.basic_geometry import Point, Surround
 from data.generators.object_recognition.canvas import Canvas
 from dsls.our_dsl.functions import dsl_functions as dsl
 
 
+#  12 funcs
 def solution_045e512c(canvas: Canvas) -> Canvas:
-    largest_object = canvas.sort_objects_by_size(used_dim='area')[-1]
-    other_objects = canvas.sort_objects_by_size(used_dim='area')[:-1]
+    largest_object = dsl.select_largest_object_by_area(canvas)
+    other_objects = dsl.select_rest_of_the_objects(canvas=canvas, obj=largest_object)
+    canvas_pos_lo = dsl.get_object_feature_canvas_pos(largest_object)
     for oo in other_objects:
-        match_positions = largest_object.match(oo, match_shape_only=True)
-        eucl_dist = dsl.furthest(largest_object.canvas_pos, match_positions)
-
-        so = copy(largest_object)
+        point = dsl.get_point_for_match_shape_furthest(largest_object, oo, match_shape_only=True, padding=Surround(0, 0, 0, 0))
+        dist, _ = dsl.furthest_point_to_point(canvas_pos_lo, point)
+        colour = dsl.get_object_feature_colour(oo)
+        obj = dsl.copy_object(largest_object)
         for _ in range(3):
-            no = copy(so)
-            no.set_new_colour(oo.colour)
-            no.translate_along_direction(eucl_dist)
-            #no.canvas_pos.transform(translation=eucl_dist)
-            canvas.add_new_object(no)
-            so = no
+            obj = dsl.object_transform_new_colour(obj, colour)
+            obj = dsl.object_transform_translate_along_direction(obj, dist)
+            canvas = dsl.add_object_to_canvas(canvas, obj)
     return canvas
 
 
+#  14 funcs
 def solution_97a05b5b(canvas: Canvas) -> Canvas:
-    largest_object = canvas.sort_objects_by_size(used_dim='area')[-1]
-    other_objects = canvas.sort_objects_by_size(used_dim='area')[:-1]
-    neg = copy(largest_object)
-    neg.negative_colour()
+    largest_object = dsl.select_largest_object_by_area(canvas)
+    other_objects = dsl.select_rest_of_the_objects(canvas=canvas, obj=largest_object)
 
-    canvas_out = Canvas(size=largest_object.size)
-    new_lo = copy(largest_object)
-    new_lo.canvas_pos = Point(0, 0, -1)
-    canvas_out.add_new_object(new_lo)
+    neg_object = dsl.object_transform_negate(largest_object)
+    lo_canvas_pos = dsl.get_object_feature_canvas_pos(largest_object)
+
+    new_oo = dsl.object_transform_translate_to_point(largest_object, Point(0, 0))
+    canvas_size = dsl.get_object_feature_size(new_oo)
+    canvas_out = dsl.make_new_canvas(size=canvas_size)
+    canvas_out = dsl.add_object_to_canvas(canvas_out, new_oo)
 
     for oo in other_objects:
-        o3 = copy(oo)
-        o3.actual_pixels[np.where(o3.actual_pixels != largest_object.colour)] = 1
-
-        match_positions = o3.match(neg, after_rotation=True, padding=Surround(1, 1, 1, 1))[0]
-
-        new = copy(oo)
-        new.rotate(match_positions[1])
-        new.translate_to(target_point=match_positions[0] - largest_object.canvas_pos)
-        #new.canvas_pos = match_positions[0] - largest_object.canvas_pos
-        canvas_out.add_new_object(new)
+        match_position, rotation = dsl.get_point_and_rotation_for_match_shape_furthest(oo, neg_object,
+                                                                                       match_shape_only=False,
+                                                                                       padding=Surround(1, 1, 1, 1))
+        oo = dsl.object_transform_rotate(oo, rotation=rotation)
+        oo_pos = dsl.subtract_points(match_position, lo_canvas_pos)
+        oo = dsl.object_transform_translate_to_point(oo, oo_pos)
+        canvas_out = dsl.add_object_to_canvas(canvas_out, oo)
 
     return canvas_out
 
 
+#  14 funcs
 def solution_b775ac94(canvas: Canvas) -> Canvas:
-    initial_list_of_objects = copy(canvas.objects)
-    out_canvas = Canvas(size=canvas.size)
+    object_list = dsl.select_rest_of_the_objects(canvas, obj=None)
+    canvas_out = dsl.copy_canvas(canvas)
 
-    for obj in initial_list_of_objects:
-        temp_canvas = Canvas(size=canvas.size, objects=[obj])
-        _ = temp_canvas.split_object_by_colour(obj)
-
-        largest_object = dsl.largest_object_by_area(temp_canvas)
-        other_objects = dsl.rest_of_the_objects(temp_canvas, largest_object)
-
-        out_canvas.objects.append(largest_object)
+    for init_obj in object_list:
+        canvas_temp = dsl.make_new_canvas_as(canvas)
+        canvas_temp = dsl.add_object_to_canvas(canvas_temp, init_obj)
+        canvas_temp = dsl.split_object_by_colour_on_canvas(canvas=canvas_temp, obj=init_obj)
+        largest_object = dsl.select_largest_object_by_area(canvas=canvas_temp)
+        other_objects = dsl.select_rest_of_the_objects(canvas=canvas_temp, obj=largest_object)
 
         for oo in other_objects:
-            distance = largest_object.distance_to_object(oo, dist_type='min')
-            lo = copy(largest_object)
-            lo.flip(axis=distance.orientation, translate=True)
-            lo.set_new_colour(oo.colour)
-            out_canvas.add_new_object(lo)
+            colour = dsl.get_object_feature_colour(oo)
+            dist = dsl.get_distance_min_between_objects(largest_object, oo)
+            largest_object = dsl.object_transform_flip_and_translate(largest_object, dist)
+            largest_object = dsl.object_transform_new_colour(largest_object, colour)
+            canvas_out.add_new_object(largest_object)
 
-    return out_canvas
+    return canvas_out
 
 
-def solution_05f2a901(canvas: Canvas):
+#  7 funcs
+def solution_05f2a901(canvas: Canvas) -> Canvas:
+    blue = dsl.select_object_of_colour(canvas, 9)
+    red = dsl.select_object_of_colour(canvas, 3)
+    dist = dsl.get_distance_touching_between_objects(red, blue)
 
-    blue = canvas.find_objects_of_colour(9)[0]
-    red = canvas.find_objects_of_colour(3)[0]
-
-    dist = red.distance_to_object(blue)
-    dist.length -= 1
-    red.translate_along_direction(dist)
-    out_canvas = Canvas(size=canvas.size)
-    out_canvas.add_new_object(blue)
-    out_canvas.add_new_object(red)
+    red = dsl.object_transform_translate_along_direction(red, dist)
+    out_canvas = dsl.make_new_canvas_as(canvas)
+    out_canvas = dsl.add_object_to_canvas(out_canvas, red)
+    out_canvas = dsl.add_object_to_canvas(out_canvas, blue)
 
     return out_canvas
